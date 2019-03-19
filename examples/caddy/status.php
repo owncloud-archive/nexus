0c9f5946ec6a5f6357000f9d9b2b0af404dc89df
Jörn Friedrich Dreyer <jfd@butonic.de>
2019-03-19 17:08:59 +0100

    fake unauthenticated status.php
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

---
+++ examples/caddy/status.php
@@ -0,0 +1,8 @@
+{
+    "installed": true,
+    "maintenance": false,
+    "version": "11.0.0.0",
+    "versionstring": "11.0.0",
+    "edition": "",
+    "fixme": "https://github.com/owncloud/nexus/issues/13"
+}
